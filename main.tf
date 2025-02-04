c5e833578f836efef883d4b2f857f572ce464cae
sathavahana <48242515+sathavahanadevops@users.noreply.github.com>
2025-02-04 18:35:07 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -21,24 +21,70 @@ resource "aws_subnet" "eks_subnet_b" {
   availability_zone = "us-east-1b"
 }
 
+resource "aws_security_group" "eks_sg" {
+  vpc_id = aws_vpc.eks_vpc.id
+  name   = "eks-cluster-sg"
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = { Name = "eks-security-group" }
+}
+
+resource "aws_security_group" "node_sg" {
+  vpc_id = aws_vpc.eks_vpc.id
+  name   = "eks-node-sg"
+
+  ingress {
+    from_port        = 1025
+    to_port          = 65535
+    protocol         = "tcp"
+    security_groups  = [aws_security_group.eks_sg.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = { Name = "eks-node-security-group" }
+}
+
 resource "aws_eks_cluster" "devops_demo" {
   name     = "devops-demo"
-  role_arn = "arn:aws:iam::443370691052:role/awsfullaccessrole" # Use your created role
+  role_arn = "arn:aws:iam::443370691052:role/awsfullaccessrole"
 
   vpc_config {
-    subnet_ids = [aws_subnet.eks_subnet_a.id, aws_subnet.eks_subnet_b.id]
+    subnet_ids         = [aws_subnet.eks_subnet_a.id, aws_subnet.eks_subnet_b.id]
+    security_group_ids = [aws_security_group.eks_sg.id]
   }
 }
 
 resource "aws_eks_node_group" "devops_demo_nodes" {
   cluster_name    = aws_eks_cluster.devops_demo.name
   node_group_name = "devops-demo-nodes"
-  node_role_arn   = "arn:aws:iam::443370691052:role/awsfullaccessrole" # Use your created role
+  node_role_arn   = "arn:aws:iam::443370691052:role/awsfullaccessrole"
   subnet_ids      = [aws_subnet.eks_subnet_a.id, aws_subnet.eks_subnet_b.id]
   instance_types  = ["t3.medium"]
+
   scaling_config {
     desired_size = 2
     max_size     = 3
     min_size     = 1
   }
+
+  depends_on = [aws_eks_cluster.devops_demo]
 }
